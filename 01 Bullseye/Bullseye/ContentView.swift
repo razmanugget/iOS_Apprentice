337261fe937fbd2c90ec19007a1eb17ad69c84da
Rami <rami_s@hotmail.com>
2019-12-12 16:39:57 +0100

    comments

--- 01 Bullseye/Bullseye/ContentView.swift
+++ 01 Bullseye/Bullseye/ContentView.swift
@@ -9,13 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+   // Properties
+   
+   // User interface views
    @State var alertIsVisible: Bool = false
-
+   
+   // User interface content and layout
    var body: some View {
+      
       VStack {
+         // Target row
          Text("hey there!")
             .fontWeight(.semibold)
             .foregroundColor(.green)
+         
+         // Slider row
+         // TODO: Add views for the slider row here
+         
+         // Button row
          Button(action: {
             print("Button pressed!")
             self.alertIsVisible = true
@@ -27,13 +38,24 @@ struct ContentView: View {
                   message: Text("This is my first pop-up."),
                   dismissButton: .default(Text("Awesome!")))
          }
+         
+         // Score row
+         // TODO: Add views for score, rounds, start and info
       }
    }
+   
+   // Methods
+   
 }
 
+
+// Preview
+// =======
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
    static var previews: some View {
       ContentView()
    }
 }
-
+#endif
