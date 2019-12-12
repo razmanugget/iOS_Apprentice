f5969919d1184550d7830152c198ea7183a01431
Rami <rami_s@hotmail.com>
2019-12-12 17:18:01 +0100

    updates

--- 01 Bullseye/Bullseye/ContentView.swift
+++ 01 Bullseye/Bullseye/ContentView.swift
@@ -18,18 +18,23 @@ struct ContentView: View {
    var body: some View {
       
       VStack {
+         Spacer()
          // Target row
          HStack {
             Text("Put the bullseye as close as you can to:")
             Text("100")
          }
          
+         Spacer()
+         
          // Slider row
          HStack {
             Text("1")
-            Slider(value: .constant(10))
+            Slider(value: /*@START_MENU_TOKEN@*/.constant(10)/*@ENDMENU_TOKEN@*/)
             Text("100")
          }
+         
+           Spacer()
          
          // Button row
          Button(action: {
@@ -44,15 +49,25 @@ struct ContentView: View {
                   dismissButton: .default(Text("Awesome!")))
          }
          
+           Spacer()
+         
          // Score row
          HStack {
             Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-            Text("Start over")
+               Text("Start over")
             }
+            Spacer()
+            Text("Score:")
+            Text("999999")
+            Spacer()
+            Text("Round:")
+            Text("999")
+            Spacer()
             Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-            Text("Info")
+               Text("Info")
             }
          }
+         .padding(.bottom, 20)
       }
    }
    
