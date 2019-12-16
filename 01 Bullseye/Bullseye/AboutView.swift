6862309ce833b96eb9191c71dd0a23b54ea2419a
Rami <rami_s@hotmail.com>
2019-12-16 11:54:33 +0100

    style to info

--- 01 Bullseye/Bullseye/AboutView.swift
+++ 01 Bullseye/Bullseye/AboutView.swift
@@ -9,13 +9,43 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+   var body: some View {
+      VStack {
+         Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+         Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
+            .modifier(AboutBodyStyle())
+         Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
+            .modifier(AboutBodyStyle())
+         Text("Enjoy!")
+            .modifier(AboutBodyStyle())
+      }
+   }
 }
 
+// MARK: - View Modifiers
+struct AboutHeadingStyle: ViewModifier {
+   func body(content: Content) -> some View {
+      content
+         .font(Font.custom("Arial Rounded MT Bold", size: 30))
+         .foregroundColor(Color.black)
+         .padding(.top, 20)
+         .padding(.bottom, 20)
+   }
+}
+struct AboutBodyStyle: ViewModifier {
+   func body(content: Content) -> some View {
+      content
+         .font(Font.custom("Arial Rounded MT Bold", size: 16))
+         .foregroundColor(Color.black)
+         .padding(.leading, 60)
+         .padding(.trailing, 60)
+         .padding(.bottom, 20)
+   }
+}
+
+
 struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutView()
-    }
+   static var previews: some View {
+      AboutView()
+   }
 }
