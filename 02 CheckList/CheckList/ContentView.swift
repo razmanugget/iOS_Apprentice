889c19908fb707b69ec29ee34b9a324ba280e8bf
Rami <rami_s@hotmail.com>
2019-12-17 11:01:54 +0100

    group

--- 02 CheckList/CheckList/ContentView.swift
+++ 02 CheckList/CheckList/ContentView.swift
@@ -13,13 +13,33 @@ struct ContentView: View {
       NavigationView {
          List {
             Section(header: Text("High priority")) {
-               Text("Walk the dog")
-               Text("Brush my teeth")
-               Text("Learn iOS development")
+               Group {
+                  Text("Walk the dog")
+                  Text("Brush my teeth")
+                  Text("Learn iOS development")
+                  Text("Make dinner")
+                  Text("Do laundry")
+                  Text("Pay bills")
+               }
+               Group {
+                  Text("Finish homework")
+                  Text("Change internet provider")
+                  Text("Read Raywenderlich.com")
+                  Text("Clean the kitchen")
+                  Text("Wash the car")
+               }
             }
             Section(header: Text("Low priority")) {
                Text("Soccer practice")
                Text("Eat ice cream")
+               Text("Take vocal lessons")
+               Text("Record hit single")
+               Text("Learn every martial art")
+               Text("Design costume")
+               Text("Design crime-fighting vehicle")
+               Text("Come up with superhero name")
+               Text("Befriend space raccoon")
+               Text("Save the world")
             }
          }
          .listStyle(GroupedListStyle())
