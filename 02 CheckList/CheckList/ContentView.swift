936e75691a568b5c89d61c1d8dd63e98ed2fea6e
Rami <rami_s@hotmail.com>
2019-12-17 11:16:09 +0100

    @State

--- 02 CheckList/CheckList/ContentView.swift
+++ 02 CheckList/CheckList/ContentView.swift
@@ -9,41 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State var checklistItems = [
+      "Walk the dog",
+      "Brush my teeth",
+      "Learn iOS development",
+      "Soccer practice",
+      "Eat ice cream"
+   ]
+   
    var body: some View {
       NavigationView {
-         List {
-            Section(header: Text("High priority")) {
-               Group {
-                  Text("Walk the dog")
-                  Text("Brush my teeth")
-                  Text("Learn iOS development")
-                  Text("Make dinner")
-                  Text("Do laundry")
-                  Text("Pay bills")
+            List {
+               Text(checklistItems[0])
+                  .onTapGesture {
+                     self.checklistItems[0] = "Take the dog to the vet"
                }
-               Group {
-                  Text("Finish homework")
-                  Text("Change internet provider")
-                  Text("Read Raywenderlich.com")
-                  Text("Clean the kitchen")
-                  Text("Wash the car")
-               }
+               Text(checklistItems[1])
+               Text(checklistItems[2])
+               Text(checklistItems[3])
+               Text(checklistItems[4])
             }
-            Section(header: Text("Low priority")) {
-               Text("Soccer practice")
-               Text("Eat ice cream")
-               Text("Take vocal lessons")
-               Text("Record hit single")
-               Text("Learn every martial art")
-               Text("Design costume")
-               Text("Design crime-fighting vehicle")
-               Text("Come up with superhero name")
-               Text("Befriend space raccoon")
-               Text("Save the world")
-            }
-         }
-         .listStyle(GroupedListStyle())
-         .navigationBarTitle("Checklist")
+            .navigationBarTitle("Checklist")
       }
    }
 }
