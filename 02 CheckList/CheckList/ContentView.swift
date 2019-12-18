5bf655b5cf2f51e79ceee4caec336a1ea2ef0df8
Rami <rami_s@hotmail.com>
2019-12-18 09:47:33 +0100

    printing list to console

--- 02 CheckList/CheckList/ContentView.swift
+++ 02 CheckList/CheckList/ContentView.swift
@@ -14,22 +14,24 @@ struct ContentView: View {
       "Brush my teeth",
       "Learn iOS development",
       "Soccer practice",
-      "Eat ice cream"
+      "Eat ice cream",
    ]
    
    var body: some View {
       NavigationView {
-            List {
-               Text(checklistItems[0])
-                  .onTapGesture {
-                     self.checklistItems[0] = "Take the dog to the vet"
-               }
-               Text(checklistItems[1])
-               Text(checklistItems[2])
-               Text(checklistItems[3])
-               Text(checklistItems[4])
-            }
-            .navigationBarTitle("Checklist")
+         List {
+            Text("Nothing to see here... yet!")
+         }
+         .navigationBarTitle("Checklist")
+         .onAppear() {
+            self.printChecklistContents()
+         }
+      }
+   }
+   
+   func printChecklistContents() {
+      for item in checklistItems {
+         print(item)
       }
    }
 }
