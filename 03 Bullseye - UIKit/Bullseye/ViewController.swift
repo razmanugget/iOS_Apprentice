37e0297333063fdd574752c493bd395b9d757b3e
Rami <rami_s@hotmail.com>
2019-12-24 12:39:42 +0100

    added random number

--- 03 Bullseye - UIKit/Bullseye/ViewController.swift
+++ 03 Bullseye - UIKit/Bullseye/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-   var currentValue: Int = 50
+   var currentValue: Int = 0
+   var targetValue = 0
+   
+   @IBOutlet weak var slider: UISlider!
+   
    
    @IBAction func showAlert() {
-      let message = "The value of the slider is: \(currentValue)"
+      let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
       
       let alert = UIAlertController(title: "Hello World",
                                     message: message,
@@ -20,6 +24,7 @@ class ViewController: UIViewController {
       let action = UIAlertAction(title: "OK", style: .default, handler: nil)
       alert.addAction(action)
       present(alert, animated: true, completion: nil)
+      startNewRound()
    }
    
    @IBAction func sliderMoved(_ slider: UISlider) {
@@ -27,9 +32,17 @@ class ViewController: UIViewController {
       print("The value of the slider is now: \(slider.value)")
    }
    
+   
+   func startNewRound() {
+      targetValue = Int.random(in: 1...100)
+      currentValue = 50
+      slider.value = Float(currentValue)
+   }
+   
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view.
+      startNewRound()
    }
 }
 
