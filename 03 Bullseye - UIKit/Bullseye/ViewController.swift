84158072021541373aa6d60167fd6daf541af7f3
Rami <rami_s@hotmail.com>
2019-12-24 10:23:35 +0100

    popup button added

--- 03 Bullseye - UIKit/Bullseye/ViewController.swift
+++ 03 Bullseye - UIKit/Bullseye/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBAction func showAlert() {
+      let alert = UIAlertController(title: "Hello World",
+                                    message: "This is my first app!",
+                                    preferredStyle: .alert)
+      let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+      alert.addAction(action)
+      present(alert, animated: true, completion: nil)
+   }
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view.
    }
-
-
 }
 
