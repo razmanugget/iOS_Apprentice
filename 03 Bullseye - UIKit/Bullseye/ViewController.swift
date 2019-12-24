46f99b988527ce332654834523a33a7e3136381c
Rami <rami_s@hotmail.com>
2019-12-24 11:16:44 +0100

    slider value shows with button

--- 03 Bullseye - UIKit/Bullseye/ViewController.swift
+++ 03 Bullseye - UIKit/Bullseye/ViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var currentValue: Int = 50
+   
    @IBAction func showAlert() {
+      let message = "The value of the slider is: \(currentValue)"
+      
       let alert = UIAlertController(title: "Hello World",
-                                    message: "This is my first app!",
+                                    message: message,
                                     preferredStyle: .alert)
-      let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+      let action = UIAlertAction(title: "OK", style: .default, handler: nil)
       alert.addAction(action)
       present(alert, animated: true, completion: nil)
+   }
+   
+   @IBAction func sliderMoved(_ slider: UISlider) {
+      currentValue = lroundf(slider.value)
+      print("The value of the slider is now: \(slider.value)")
    }
    
    override func viewDidLoad() {
