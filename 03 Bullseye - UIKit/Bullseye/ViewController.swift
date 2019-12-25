d7e27f3f67c61c92fc5a07a34ec480d6f19b6314
Rami <rami_s@hotmail.com>
2019-12-25 12:32:07 +0100

    testing new design

--- 03 Bullseye - UIKit/Bullseye/ViewController.swift
+++ 03 Bullseye - UIKit/Bullseye/ViewController.swift
@@ -11,38 +11,62 @@ import UIKit
 class ViewController: UIViewController {
    var currentValue: Int = 0
    var targetValue = 0
+   var score = 0
+   var round = 1
+   
+   // computed
+//   var sliderTargetDifference: Int {
+//      abs(sliderValueRounded - self.target)
+//   }
    
    @IBOutlet weak var slider: UISlider!
+   @IBOutlet weak var targetLabel: UILabel!
+   @IBOutlet weak var scoreLabel: UILabel!
+   @IBOutlet weak var roundLabel: UILabel!
    
    
    @IBAction func showAlert() {
-      let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
-      
-      let alert = UIAlertController(title: "Hello World",
-                                    message: message,
+      let alert = UIAlertController(title: "Awesome",
+                                    message: scoringMessage(),
                                     preferredStyle: .alert)
       let action = UIAlertAction(title: "OK", style: .default, handler: nil)
       alert.addAction(action)
       present(alert, animated: true, completion: nil)
       startNewRound()
    }
-   
    @IBAction func sliderMoved(_ slider: UISlider) {
       currentValue = lroundf(slider.value)
       print("The value of the slider is now: \(slider.value)")
+   }
+   @IBAction func startOver() {
+      score = 0
+      round = 0
+      resetSliderAndTarget()
    }
    
    
    func startNewRound() {
       targetValue = Int.random(in: 1...100)
-      currentValue = 50
+      targetLabel.text = String(targetValue)
+      currentValue = 80
       slider.value = Float(currentValue)
+      
    }
-   
+   func updateLabels() {
+      targetLabel.text = String(targetValue)
+   }
+   func resetSliderAndTarget() {
+      slider.value = 50
+      targetValue = Int.random(in: 1...100)
+      print(targetValue)
+   }
+   func scoringMessage() -> String {
+      return "The slider's value is \(slider.value).\n" +
+          "The target value is \(targetValue).\n"
+    }
    
    override func viewDidLoad() {
       super.viewDidLoad()
       startNewRound()
    }
 }
-
