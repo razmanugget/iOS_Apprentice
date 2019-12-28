6fff008f5c16c4bccd6be5fd12286ce8eb94c811
Rami <rami_s@hotmail.com>
2019-12-28 11:41:44 +0100

    table hooked up

--- 03 Bullseye - UIKit/Bullseye/HighScoresViewController.swift
+++ 03 Bullseye - UIKit/Bullseye/HighScoresViewController.swift
@@ -9,82 +9,47 @@
 import UIKit
 
 class HighScoresViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+   // MARK: - Table view data source
+   //   override func numberOfSections(in tableView: UITableView) -> Int {
+   //      return 0
+   //   }
+   
+   override func tableView(_ tableView: UITableView,
+                           numberOfRowsInSection section: Int)
+      -> Int {
+         return 5
+   }
+   
+   override func tableView(_ tableView: UITableView,
+                           cellForRowAt indexPath: IndexPath)
+      -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(
+            withIdentifier: "HighScoreItem", for: indexPath)
+         
+         let nameLabel = cell.viewWithTag(1000) as! UILabel
+         let scoreLabel = cell.viewWithTag(2000) as! UILabel
+         if indexPath.row == 0 {
+            nameLabel.text = "Raz"
+            scoreLabel.text = "50000"
+         } else if indexPath.row == 1 {
+            nameLabel.text = "Manda"
+            scoreLabel.text = "10000"
+         } else if indexPath.row == 2 {
+            nameLabel.text = "Joey"
+            scoreLabel.text = "5000"
+         } else if indexPath.row == 3 {
+            nameLabel.text = "Adam"
+            scoreLabel.text = "1000"
+         } else if indexPath.row == 4 {
+            nameLabel.text = "Eli"
+            scoreLabel.text = "500"
+         }
+         return cell
+   }
+   
 }
