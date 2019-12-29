bb6642c267954196d656b8d6564754f7fa4cbfda
Rami <rami_s@hotmail.com>
2019-12-29 13:30:21 +0100

    added dummy code

---
+++ 03 Bullseye - UIKit/Bullseye/HighScoreItem.swift
@@ -0,0 +1,14 @@
+//
+//  HighScoreItem.swift
+//  Bullseye
+//
+//  Created by Rami on 12/29/19.
+//  Copyright © 2019 Lyfebug. All rights reserved.
+//
+
+import Foundation
+
+class HighScoreItem {
+   var name = ""
+   var score = 0
+}
--- 03 Bullseye - UIKit/Bullseye/HighScoresViewController.swift
+++ 03 Bullseye - UIKit/Bullseye/HighScoresViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class HighScoresViewController: UITableViewController {
+   var row0item = HighScoreItem()
+   var row1item = HighScoreItem()
+   var row2item = HighScoreItem()
+   var row3item = HighScoreItem()
+   var row4item = HighScoreItem()
    
-   override func viewDidLoad() {
-      super.viewDidLoad()
-   }
+   
    
    // MARK: - Table view data source
-   //   override func numberOfSections(in tableView: UITableView) -> Int {
-   //      return 0
-   //   }
-   
    override func tableView(_ tableView: UITableView,
                            numberOfRowsInSection section: Int)
       -> Int {
          return 5
    }
-   
    override func tableView(_ tableView: UITableView,
                            cellForRowAt indexPath: IndexPath)
       -> UITableViewCell {
@@ -34,22 +32,40 @@ class HighScoresViewController: UITableViewController {
          let nameLabel = cell.viewWithTag(1000) as! UILabel
          let scoreLabel = cell.viewWithTag(2000) as! UILabel
          if indexPath.row == 0 {
-            nameLabel.text = "Raz"
-            scoreLabel.text = "50000"
+            nameLabel.text = row0item.name
+            scoreLabel.text = String(row0item.score)
          } else if indexPath.row == 1 {
-            nameLabel.text = "Manda"
-            scoreLabel.text = "10000"
+            nameLabel.text = row1item.name
+            scoreLabel.text = String(row1item.score)
          } else if indexPath.row == 2 {
-            nameLabel.text = "Joey"
-            scoreLabel.text = "5000"
+            nameLabel.text = row2item.name
+            scoreLabel.text = String(row2item.score)
          } else if indexPath.row == 3 {
-            nameLabel.text = "Adam"
-            scoreLabel.text = "1000"
+            nameLabel.text = row3item.name
+            scoreLabel.text = String(row3item.score)
          } else if indexPath.row == 4 {
-            nameLabel.text = "Eli"
-            scoreLabel.text = "500"
+            nameLabel.text = row4item.name
+            scoreLabel.text = String(row4item.score)
          }
          return cell
    }
+   override func tableView (_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+      tableView.deselectRow(at: indexPath, animated: true)
+   }
    
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      row0item.name = "The reader of this book"
+      row0item.score = 50000
+      row1item.name = "Manda"
+      row1item.score = 10000
+      row2item.name = "Joey"
+      row2item.score = 5000
+      row3item.name = "Adam"
+      row3item.score = 1000
+      row4item.name = "Eli"
+      row4item.score = 500
+   }
 }
