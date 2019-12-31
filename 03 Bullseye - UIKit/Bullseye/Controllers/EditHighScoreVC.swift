c0ad1366746f60c3c64922e8bdfbff936c4d628e
Rami <rami_s@hotmail.com>
2019-12-31 15:49:50 +0100

    error handling

--- 03 Bullseye - UIKit/Bullseye/Controllers/EditHighScoreVC.swift
+++ 03 Bullseye - UIKit/Bullseye/Controllers/EditHighScoreVC.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class EditHighScoreVC: UITableViewController {
-   
+class EditHighScoreVC: UITableViewController, UITextFieldDelegate {
    
    @IBOutlet weak var textField: UITextField!
+   @IBOutlet weak var doneBarButton: UIBarButtonItem!
+   
    
    // MARK: - IBActions
    @IBAction func cancel() {
@@ -24,12 +25,37 @@ class EditHighScoreVC: UITableViewController {
    }
    
    
+   // MARK: - TableView Delegate
+   func textField(_ textField: UITextField,
+                  shouldChangeCharactersIn range: NSRange,
+                  replacementString string: String)
+      -> Bool {
+         let oldText = textField.text!
+         let stringRange = Range(range, in: oldText)!
+         let newText = oldText.replacingCharacters(in: stringRange,
+                                                   with: string)
+         if newText.isEmpty {
+            doneBarButton.isEnabled = false
+         } else {
+            doneBarButton.isEnabled = true
+         }
+         return true
+   }
+   
+   
    //MARK: - TableView Actions
    // keeps the row from being selected when tapped
    override func tableView(_ tableView: UITableView,
                            willSelectRowAt indexPath: IndexPath)
       -> IndexPath? {
          return nil
+   }
+   
+   
+   // MARK: - View Controller Life Cycle
+   override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      textField.becomeFirstResponder()
    }
    
    override func viewDidLoad() {
