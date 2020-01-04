97db949ab5a2b05be14a21ca77f3718fc7072fc7
Rami <rami_s@hotmail.com>
2020-01-04 12:45:14 +0100

    MyLocation start

---
+++ 04 MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Rami on 1/4/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
---
+++ 04 MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Rami on 1/4/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
