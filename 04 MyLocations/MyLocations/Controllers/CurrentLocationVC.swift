c2a36ac336963126702b7b706a017e2b75079a85
Rami <rami_s@hotmail.com>
2020-01-05 13:33:29 +0100

    added UI and constraints

--- 04 MyLocations/MyLocations/Controllers/CurrentLocationVC.swift
+++ 04 MyLocations/MyLocations/Controllers/CurrentLocationVC.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class CurrentLocationVC: UIViewController {
-
+   
+   // MARK: - IBOutlets
+   @IBOutlet weak var messageLabel: UILabel!
+   @IBOutlet weak var latitudeLabel: UILabel!
+   @IBOutlet weak var longitudeLabel: UILabel!
+   @IBOutlet weak var addressLabel: UILabel!
+   @IBOutlet weak var tagButton: UIButton!
+   @IBOutlet weak var getButton: UIButton!
+   
+   
+   // MARK: - IBActions
+   @IBAction func getLocation() {
+      
+   }
+   
+   
+   
+   
+   
+   // MARK: - View Controller Life Cycle
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view.
    }
-
-
+   
 }
 
