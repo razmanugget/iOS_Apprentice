cb9d031c157bc7e5d6be852b5fd4386ca5c0ad03
Rami <rami_s@hotmail.com>
2020-01-09 15:06:22 +0100

    HUD check finished

--- 04 MyLocations/MyLocations/Views/HudView.swift
+++ 04 MyLocations/MyLocations/Views/HudView.swift
@@ -38,5 +38,25 @@ class HudView: UIView {
       let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
       UIColor(white: 0.3, alpha: 0.8).setFill()
       roundedRect.fill()
+      
+      // draw checkmark
+      if let image = UIImage(named: "Checkmark") {
+         let imagePoint = CGPoint(
+            x: center.x - round(image.size.width / 2),
+            y: center.y - round(image.size.height / 2) - boxHeight / 8)
+         image.draw(at: imagePoint)
+      }
+      
+      // draw the text
+      let attribs = [
+         NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+         NSAttributedString.Key.foregroundColor: UIColor.white ]
+      let textSize = text.size(withAttributes: attribs)
+      
+      let textPoint = CGPoint(
+         x: center.x - round(textSize.width / 2),
+         y: center.y - round(textSize.height / 2) + boxHeight / 4)
+      
+      text.draw(at: textPoint, withAttributes: attribs)
    }
 }
