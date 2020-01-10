12877c86bb6b5d5b730d8f598c1fe3cbeb91b130
Rami <rami_s@hotmail.com>
2020-01-10 16:43:21 +0100

    added coredata module

---
+++ 04 MyLocations/MyLocations/DataModels/Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Rami on 1/10/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
