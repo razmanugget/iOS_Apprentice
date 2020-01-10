a78799b39089f46f457c07a05950ae8b903d3480
Rami <rami_s@hotmail.com>
2020-01-10 18:25:26 +0100

    error with appDelegate

--- 04 MyLocations/MyLocations/Support Files/AppDelegate.swift
+++ 04 MyLocations/MyLocations/Support Files/AppDelegate.swift
@@ -11,8 +11,22 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+   
+   var window: UIWindow?
+   
+   lazy var persistentContainer: NSPersistentContainer = {
+      let container = NSPersistentContainer(name: "DataModel")
+      container.loadPersistentStores { (storeDescription, error) in
+         if let error = error {
+            fatalError("Could not load data store: \(error)")
+         }
+      }
+      return container
+   }()
 
-
+   lazy var managedObjectContext: NSManagedObjectContext = persistentContainer.viewContext
+   
+   
 
    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
       // Override point for customization after application launch.
