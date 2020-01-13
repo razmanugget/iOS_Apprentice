b0f726f99070201adc2a82cd242882654f1c4cb0
Rami <rami_s@hotmail.com>
2020-01-13 12:39:15 +0100

    optional error

--- 04 MyLocations/MyLocations/Controllers/Functions.swift
+++ 04 MyLocations/MyLocations/Controllers/Functions.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+let applicationDocumentsDirectory: URL = {
+   let paths = FileManager.default.urls(for: .documentDirectory,
+                                        in: .userDomainMask
+   )
+   return paths[0]
+}()
+
+
 func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
    print("getting delayed")
    DispatchQueue.main.asyncAfter(deadline: .now() + seconds,
--- 04 MyLocations/MyLocations/Support Files/AppDelegate.swift
+++ 04 MyLocations/MyLocations/Support Files/AppDelegate.swift
@@ -22,47 +22,55 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       }
       return container
    }()
-
+   
    lazy var managedObjectContext: NSManagedObjectContext = persistentContainer.viewContext
    
    
-
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      // Override point for customization after application launch.
-      return true
+   func application(_ application: UIApplication,
+                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+      -> Bool {
+         let tabController = window!.rootViewController as! UITabBarController
+         if let tabViewControllers = tabController.viewControllers {
+            let navController = tabViewControllers[0] as! UINavigationController
+            let controller = navController.viewControllers.first as! CurrentLocationVC
+            controller.managedObjectContext = managedObjectContext
+         }
+         print(applicationDocumentsDirectory)
+         return true
    }
-
-   // MARK: UISceneSession Lifecycle
-
+   
+   
+   // MARK: - UISceneSession Lifecycle
    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
       // Called when a new scene session is being created.
       // Use this method to select a configuration to create the new scene with.
       return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
    }
-
+   
    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
       // Called when the user discards a scene session.
       // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
       // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
    }
-
+   
+   
    // MARK: - Core Data stack
-
+   
    // MARK: - Core Data Saving support
-
+   
    func saveContext () {
-       let context = persistentContainer.viewContext
-       if context.hasChanges {
-           do {
-               try context.save()
-           } catch {
-               // Replace this implementation with code to handle the error appropriately.
-               // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-               let nserror = error as NSError
-               fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-           }
-       }
+      let context = persistentContainer.viewContext
+      if context.hasChanges {
+         do {
+            try context.save()
+         } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+         }
+      }
    }
-
+   
 }
 
