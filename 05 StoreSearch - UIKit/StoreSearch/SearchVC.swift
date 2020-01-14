129f47ede0bb2ff2c0ab8cd30266ce628d30d06b
Rami <rami_s@hotmail.com>
2020-01-14 19:17:53 +0100

    search works

--- 05 StoreSearch - UIKit/StoreSearch/SearchVC.swift
+++ 05 StoreSearch - UIKit/StoreSearch/SearchVC.swift
@@ -12,16 +12,42 @@ import UIKit
 // MARK: - Enums | Extensions | Protoc
 extension SearchVC: UISearchBarDelegate {
    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-      print("The search text is: '\(searchBar.text!)'")
+      // dismiss the keyboard
+      searchBar.resignFirstResponder()
+      searchResults = []
+      for i in 0...2 {
+         searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+      }
+      tableView.reloadData()
    }
 }
 
 extension SearchVC: UITableViewDelegate, UITableViewDataSource {
-   
+   func tableView(_ tableView: UITableView,
+                  numberOfRowsInSection section: Int)
+      -> Int {
+         return searchResults.count
+   }
+   func tableView(_ tableView: UITableView,
+                  cellForRowAt indexPath: IndexPath)
+      -> UITableViewCell {
+         let cellIdentifier = "SearchResultCell"
+         
+         var cell: UITableViewCell! = tableView.dequeueReusableCell(
+            withIdentifier: cellIdentifier)
+         if cell == nil {
+            cell = UITableViewCell(style: .default,
+                                   reuseIdentifier: cellIdentifier)
+         }
+         cell.textLabel!.text = searchResults[indexPath.row]
+         return cell
+   }
 }
 
 
 class SearchVC: UIViewController {
+   var searchResults = [String]()
+   
    
    @IBOutlet weak var searchBar: UISearchBar!
    @IBOutlet weak var tableView: UITableView!
