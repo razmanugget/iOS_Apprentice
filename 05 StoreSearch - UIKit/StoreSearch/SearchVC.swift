e7a7614e90cd385faf84a753c1944c1e73de42ae
Rami <rami_s@hotmail.com>
2020-01-15 15:59:05 +0100

    new formatting for nothing found cell

--- 05 StoreSearch - UIKit/StoreSearch/SearchVC.swift
+++ 05 StoreSearch - UIKit/StoreSearch/SearchVC.swift
@@ -46,17 +46,20 @@ extension SearchVC: UITableViewDelegate, UITableViewDataSource {
    func tableView(_ tableView: UITableView,
                   cellForRowAt indexPath: IndexPath)
       -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(
-            withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
          if searchResults.count == 0 {
-            cell.nameLabel.text = "(Nothing found)"
-            cell.artistNameLabel.text = ""
+            return tableView.dequeueReusableCell(
+               withIdentifier: TableView.CellIdentifiers.nothingFoundCell,
+               for: indexPath)
          } else {
+            let cell = tableView.dequeueReusableCell(
+               withIdentifier: TableView.CellIdentifiers.searchResultCell,
+               for: indexPath) as! SearchResultCell
+            
             let searchResult = searchResults[indexPath.row]
             cell.nameLabel.text = searchResult.name
             cell.artistNameLabel.text = searchResult.artistName
+            return cell
          }
-         return cell
    }
    func tableView(_ tableView: UITableView,
                   didSelectRowAt indexPath: IndexPath) {
@@ -82,6 +85,7 @@ class SearchVC: UIViewController {
    struct TableView {
       struct CellIdentifiers {
          static let searchResultCell = "SearchResultCell"
+         static let nothingFoundCell = "NothingFoundCell"
       }
    }
    
@@ -97,8 +101,10 @@ class SearchVC: UIViewController {
       // allow for 20pt status bar, 44pt search bar
       tableView.contentInset = UIEdgeInsets(top: 64, left: 0,
                                             bottom: 0, right: 0)
-      let cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
+      var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
       tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+      cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
+      tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
    }
    
 }
