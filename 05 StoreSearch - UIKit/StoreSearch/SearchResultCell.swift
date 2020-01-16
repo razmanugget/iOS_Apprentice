98edd5e81b086330754d6510e7d025313ad3607b
Rami <rami_s@hotmail.com>
2020-01-16 13:31:27 +0100

    app icons

--- 05 StoreSearch - UIKit/StoreSearch/SearchResultCell.swift
+++ 05 StoreSearch - UIKit/StoreSearch/SearchResultCell.swift
@@ -17,7 +17,10 @@ class SearchResultCell: UITableViewCell {
    
    override func awakeFromNib() {
       super.awakeFromNib()
-      // Initialization code
+      let selectedView = UIView(frame: CGRect.zero)
+      selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255,
+                                             blue: 160/255, alpha: 0.5)
+      selectedBackgroundView = selectedView
    }
    
    override func setSelected(_ selected: Bool, animated: Bool) {
