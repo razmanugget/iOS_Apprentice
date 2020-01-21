28a764856ae60fe8b998c77b35ee35c018c3e525
Rami <rami_s@hotmail.com>
2020-01-21 12:48:37 +0100

    41 - allow for http requests

--- 05 StoreSearch - UIKit/StoreSearch/SearchResultCell.swift
+++ 05 StoreSearch - UIKit/StoreSearch/SearchResultCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 
 class SearchResultCell: UITableViewCell {
+   var downloadTask: URLSessionDownloadTask?
    
    @IBOutlet weak var nameLabel: UILabel!
    @IBOutlet weak var artistNameLabel: UILabel!
@@ -25,8 +26,18 @@ class SearchResultCell: UITableViewCell {
          artistNameLabel.text = String(format: "%@ (%@)",
                                        result.artist, result.type)
       }
+      artworkImageView.image = UIImage(named: "Placeholder")
+      if let smallURL = URL(string: result.imageSmall) {
+         downloadTask = artworkImageView.loadImage(url: smallURL)
+      }
    }
 
+   override func prepareForReuse() {
+      print("hello before prepare for reuse")
+      super.prepareForReuse()
+      downloadTask?.cancel()
+      downloadTask = nil
+   }
    
    override func awakeFromNib() {
       super.awakeFromNib()
--- 05 StoreSearch - UIKit/StoreSearch/UIImageView+DownloadImage.swift
+++ 05 StoreSearch - UIKit/StoreSearch/UIImageView+DownloadImage.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Lyfebug. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+   func loadImage(url: URL) -> URLSessionDownloadTask {
+      let session = URLSession.shared
+      // save the downloaded file to a temp location
+      let downloadTask = session.downloadTask(
+         with: url, completionHandler: { [weak self] url, response, error in
+            if error == nil,
+               let url = url,
+               let data = try? Data(contentsOf: url),
+               let image = UIImage(data: data) {
+               DispatchQueue.main.async {
+                  if let weakSelf = self {
+                     weakSelf.image = image
+                  }
+               }
+            }
+      })
+      // start with resume gives opportunity to call cancel
+      downloadTask.resume()
+      return downloadTask
+   }
+}
