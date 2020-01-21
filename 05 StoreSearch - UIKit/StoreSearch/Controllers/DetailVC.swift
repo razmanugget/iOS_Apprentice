8188ba7846d85baadd91355a158b67ded19e8fc0
Rami <rami_s@hotmail.com>
2020-01-21 15:56:42 +0100

    42 - file structure cleanup and detail controller add

---
+++ 05 StoreSearch - UIKit/StoreSearch/Controllers/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailVC.swift
+//  StoreSearch
+//
+//  Created by Rami on 1/21/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
