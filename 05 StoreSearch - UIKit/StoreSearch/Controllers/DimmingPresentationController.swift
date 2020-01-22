2a44f04dc851058724153ffe46a32569130119f5
Rami <rami_s@hotmail.com>
2020-01-22 20:49:29 +0100

    42 - dimming controller added

---
+++ 05 StoreSearch - UIKit/StoreSearch/Controllers/DimmingPresentationController.swift
@@ -0,0 +1,15 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Rami on 1/22/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+   override var shouldRemovePresentersView: Bool {
+      return false
+   }
+}
