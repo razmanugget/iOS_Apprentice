689762120d7926eefbf49da4a1388805e2aafafa
Rami <rami_s@hotmail.com>
2020-02-06 12:09:46 +0100

    46 - web request now is internationalized -> shows euros, ","

--- 05 StoreSearch - UIKit/StoreSearch/Controllers/Search.swift
+++ 05 StoreSearch - UIKit/StoreSearch/Controllers/Search.swift
@@ -72,13 +72,19 @@ class Search {
    }
    
    // MARK: - Helper Methods
-   func iTunesURL(searchText: String, category: Category) -> URL {
+   private func iTunesURL(searchText: String, category: Category) -> URL {
+      let locale = Locale.autoupdatingCurrent
+      let language = locale.identifier
+      let countryCode = locale.regionCode ?? "US"
+      
       let kind = category.type
       let encodedText = searchText.addingPercentEncoding(
          withAllowedCharacters: CharacterSet.urlQueryAllowed)!
       let urlString =  "https://itunes.apple.com/search?" +
-      "term=\(encodedText)&limit=200&entity=\(kind)"
+         "term=\(encodedText)&limit=200&entity=\(kind)" +
+      "&lang=\(language)&country=\(countryCode)"
       let url = URL(string: urlString)
+      print("URL:\(url!)")
       return url!
    }
    
