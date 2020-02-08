a2f923b1f559bb1603a3819f73d554040f551dab
Rami <rami_s@hotmail.com>
2020-02-08 13:07:20 +0100

    47 - dark mode

--- 05 StoreSearch - UIKit/StoreSearch/Controllers/DetailVC.swift
+++ 05 StoreSearch - UIKit/StoreSearch/Controllers/DetailVC.swift
@@ -163,8 +163,16 @@ class DetailVC: UIViewController {
    override func viewDidLoad() {
       super.viewDidLoad()
      
-      view.tintColor = UIColor(red: 20/255, green: 160/255,
-                               blue: 160/255, alpha: 1)
+      
+      if (traitCollection.userInterfaceStyle == .light) {
+         view.tintColor = UIColor(red: 20/255, green: 160/255,
+                                     blue: 160/255, alpha: 1)
+      } else {
+         view.tintColor = UIColor(red: 140/255, green: 140/255,
+                                     blue: 240/255, alpha: 1)
+      }
+   
+      
       popupView.layer.cornerRadius = 10
       
       if isPopUp {
